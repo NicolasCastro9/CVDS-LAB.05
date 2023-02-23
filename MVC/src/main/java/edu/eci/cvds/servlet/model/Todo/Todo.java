c0c70210c58fe53a92353b9740c16573b86da13a
sergio-g-v <sergio.gonzalez-v@mail.escuelaing.edu.co>
2023-02-23 18:34:48 -0500

    todo

---
+++ MVC/src/main/java/edu/eci/cvds/servlet/model/Todo/Todo.java
@@ -0,0 +1,10 @@
+package edu.eci.cvds.servlet.model.Todo;
+
+public class Todo{
+    static int number;
+    public Todo(){}
+
+    public int getter(){return number;}
+    public int setter(int numero){number = numero;}
+} 
+
